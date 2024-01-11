dcbf5b7e17d26843fccce92d47d510d0fe678d39
Blueturboguy07 <114044325+Blueturboguy07@users.noreply.github.com>
2024-01-11 16:30:50 -0600

    made BSTNode inner class AND recursive size method

---
+++ MyBST.java
@@ -0,0 +1,30 @@
+public class MyBST {
+
+    BSTNode root;
+
+    private class BSTNode {
+        Integer val;
+        BSTNode left, right;
+
+        public BSTNode(Integer val) {
+            this.val = val;
+            left = right = null;
+        }
+        @Override
+        public String toString() { return"" + this.val; }
+    }
+
+    public int size() {
+        return sizeHelper(0, root);
+    }
+    
+    private int sizeHelper(int countSoFar, BSTNode currentNode) {
+        if (currentNode == null) {
+            return countSoFar;
+        } else {
+            return 1 + sizeHelper(countSoFar, currentNode.left) + sizeHelper(countSoFar, currentNode.right);
+        }
+    }
+    
+
+}
